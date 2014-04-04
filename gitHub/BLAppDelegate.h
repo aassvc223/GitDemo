34feed22d6d17d9a024949d33c1fdde897422d3b
BRUCE LAU <brucelau@BRUCEtekiMacBook-Air.local>
2014-04-04 14:55:54 +0800

    Initial Commit

---
+++ gitHub/BLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BLAppDelegate.h
+//  gitHub
+//
+//  Created by BRUCE LAU on 14-4-4.
+//  Copyright (c) 2014年 BRUCE LAU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
