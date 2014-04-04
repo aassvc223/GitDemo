e4a2b654690bfcdb45aab36ff6dc1fb39dc4e273
BRUCE LAU <brucelau@BRUCEtekiMacBook-Air.local>
2014-04-04 15:01:40 +0800

    add 1 line

--- gitHub/BLAppDelegate.m
+++ gitHub/BLAppDelegate.m
@@ -16,6 +16,8 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    NSLog(@"123");
     return YES;
 }
 
